e77bddd704a122e6a838793508665507e44db05a
RAMAKRISHNA1009 <ramak5135@gmail.com>
2023-08-02 19:15:36 +0530

    Task4 Completed

--- Task 4 - Javascript/Ramakrishna_Task4/script.js
+++ Task 4 - Javascript/Ramakrishna_Task4/script.js
@@ -23,16 +23,62 @@ function createProductElement(product) {
       : product.title;
 
   const productContent = `
-        <div class="position-relative">
-            <span class="heart-icon"><i class="bi bi-heart"></i></span>
-            <img src="${product.image}" alt="${product.title}">
-            <div class="product-info">
-                <h5 class="product-title">${truncatedTitle}</h5>
-                <p class="product-price">Price: $${product.price}</p>
-            </div>
-        </div>
+  <div class="position-relative">
+         <span class="heart-icon"><i class="bi bi-heart"></i></span>
+         <img src="${product.image}" alt="${product.title}">
+         <div class="product-info">
+           <div class="title-and-icon">
+             <h5 class="product-title">${truncatedTitle}</h5>
+           </div>
+           <p class="product-price">Price: $${product.price}</p>
+           <div style="display: flex; align-items: center;">
+             <div class="circle" style="background-color: gray;"></div>
+             <div class="circle" style="background-color: yellow;"></div>
+             <div class="circle" style="background-color: pink;"></div>
+           </div>
+         </div>
+       </div>
     `;
 
   productDiv.innerHTML = productContent;
   return productDiv;
 }
+
+
+// function createProductElement(product) {
+//   const productDiv = document.createElement("div");
+//   productDiv.classList.add("product");
+//   const truncatedTitle =
+//     product.title.length > 18
+//       ? product.title.substring(0, 18) + "..."
+//       : product.title;
+
+//   const productContent = `
+//     <div class="position-relative">
+//       <span class="heart-icon"><i class="bi bi-heart"></i></span>
+//       <img src="${product.image}" alt="${product.title}">
+//       <div class="product-info">
+//         <div class="title-and-icon">
+//           <h5 class="product-title">${truncatedTitle}</h5>
+//           <div class="plus-icon">
+//             <span class="plus-icon-content"><i class="bi bi-plus"></i></span>
+//           </div>
+//         </div>
+//         <p class="product-price">Price: $${product.price}</p>
+//         <div style="display: flex; align-items: center;">
+//           <div class="circle" style="background-color: gray;"></div>
+//           <div class="circle" style="background-color: yellow;"></div>
+//           <div class="circle" style="background-color: pink;"></div>
+//         </div>
+//       </div>
+//     </div>
+//   `;
+
+//   productDiv.innerHTML = productContent;
+//   return productDiv;
+// }
+
+
+
+
+
